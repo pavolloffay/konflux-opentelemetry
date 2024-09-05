5c5bddd9afcc0ebbe300acfd96eccb51c6252776
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-05 15:05:13 +0000

    Update operator to 5fe1b91

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -2,7 +2,7 @@
 
 export OTEL_COLLECTOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/collector@sha256:23e6035195abea23a0aac27cde535cd27bc6b5e6a2175ecc6992c450afa93a69"
 export OTEL_TARGET_ALLOCATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/otel-target-allocator@sha256:f78aeaf39d569020ce1ce6cce49e39fb1a6c3acf674848b24bfd68875c080d18"
-export OTEL_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/operator@sha256:d85433fa0a2ac7689772ddcd2680cae73c89b317346ee4b3124f8725d9820ba0"
+export OTEL_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/operator@sha256:5fe1b915b3784e6df2b49cc1b9a419d88afd03b992d91559dab77005cd76eab7"
 
 export CSV_FILE=/manifests/opentelemetry-operator.clusterserviceversion.yaml
 
