232c6a46847c372a6b75bcf704e2183931e79e4e
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-11 07:11:33 +0000

    Update otel-target-allocator to 5281f21

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -5,7 +5,7 @@ set -e
 # The pullspec should be image index, check if all architectures are there with: skopeo inspect --raw docker://$IMG | jq
 export OTEL_COLLECTOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/otel-collector@sha256:bf40ee8863f69a8939a91af563359760932f361b8954d7a66c6254b18a08216a"
 # Separate due to merge conflicts
-export OTEL_TARGET_ALLOCATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/otel-target-allocator@sha256:9d21704e8468858afcbaea804f662c6360f625d38499d4baa14b64ddbc3f5894"
+export OTEL_TARGET_ALLOCATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/otel-target-allocator@sha256:5281f2113a08d62e51449bc06401acbf83b8de4b4b262474320fbfac0e0dc329"
 # Separate due to merge conflicts
 export OTEL_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/otel-operator@sha256:c7abd515a2ec4216c6f1ac3ee63ab9450ada9e0cd6a076b88d23b755263a0113"
 
