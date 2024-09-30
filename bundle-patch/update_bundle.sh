9c236c8bbcbc2ca156c289642a3151b1657e8d4f
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-09-30 17:17:50 +0000

    Update otel-collector to 5cd7df9
    Signed-off-by: red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>

--- bundle-patch/update_bundle.sh
+++ bundle-patch/update_bundle.sh
@@ -3,7 +3,7 @@
 set -e
 
 # The pullspec should be image index, check if all architectures are there with: skopeo inspect --raw docker://$IMG | jq
-export OTEL_COLLECTOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/opentelemetry-collector@sha256:fbeb64f818651474b12ad10c4a50fe69373fca8ea9161913c9cc6a3321045194"
+export OTEL_COLLECTOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/opentelemetry-collector@sha256:5cd7df91d5dd606d753729e28f30b4d7032e89d635c4362cfc02b8f7ab8a04a5"
 # Separate due to merge conflicts
 export OTEL_TARGET_ALLOCATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/rhosdt-tenant/otel/opentelemetry-target-allocator@sha256:39a811a2cec9a93eb087f73f0e772435b95e9c4356971f3a72376531a1afde77"
 # Separate due to merge conflicts
